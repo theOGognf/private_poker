6048057fb1732b5469e2c07a32ec1bb6cf82e476
andrew <akberger20@gmail.com>
2024-09-21 17:10:26 -0500

    reorging pp_client app

--- pp_client/src/app.rs
+++ pp_client/src/app.rs
@@ -35,6 +35,9 @@ use std::{
     time::{Duration, Instant},
 };
 
+mod widgets;
+use widgets::{UserInput, ScrollableList};
+
 pub const MAX_LOG_RECORDS: usize = 1024;
 pub const POLL_TIMEOUT: Duration = Duration::from_millis(100);
 
@@ -140,153 +143,6 @@ impl From<Record> for ListItem<'_> {
 
         let content = Line::from(msg);
         ListItem::new(content)
-    }
-}
-
-/// Manages terminal messages and the terminal view position.
-struct ScrollableList {
-    list_items: VecDeque<ListItem<'static>>,
-    list_state: ListState,
-    scroll_state: ScrollbarState,
-}
-
-impl ScrollableList {
-    pub fn jump_to_first(&mut self) {
-        self.list_state.scroll_down_by(MAX_LOG_RECORDS as u16);
-        self.scroll_state.first();
-    }
-
-    pub fn jump_to_last(&mut self) {
-        self.list_state.scroll_up_by(MAX_LOG_RECORDS as u16);
-        self.scroll_state.last();
-    }
-
-    pub fn move_down(&mut self) {
-        self.list_state.scroll_up_by(1);
-        if self.list_state.selected().is_some() {
-            self.scroll_state.scroll(ScrollDirection::Forward);
-        }
-    }
-
-    pub fn move_up(&mut self) {
-        self.list_state.scroll_down_by(1);
-        if self.list_state.selected().is_some() {
-            self.scroll_state.scroll(ScrollDirection::Backward);
-        }
-    }
-
-    pub fn new() -> Self {
-        Self {
-            list_items: VecDeque::with_capacity(MAX_LOG_RECORDS),
-            list_state: ListState::default(),
-            scroll_state: ScrollbarState::new(0),
-        }
-    }
-
-    pub fn push(&mut self, item: ListItem<'static>) {
-        if self.list_items.len() == MAX_LOG_RECORDS {
-            self.list_items.pop_back();
-        }
-        self.list_items.push_front(item);
-        self.scroll_state = self.scroll_state.content_length(self.list_items.len());
-        self.move_down();
-    }
-}
-
-/// Manages user inputs at the terminal.
-struct UserInput {
-    /// Position of cursor in the input box.
-    char_idx: usize,
-    /// Current value of the input box.
-    value: String,
-}
-
-impl UserInput {
-    pub fn backspace(&mut self) {
-        // Method "remove" is not used on the saved text for deleting the selected char.
-        // Reason: Using remove on String works on bytes instead of the chars.
-        // Using remove would require special care because of char boundaries.
-        if self.char_idx != 0 {
-            // Getting all characters before the selected character.
-            let before_char_to_delete = self.value.chars().take(self.char_idx - 1);
-            // Getting all characters after selected character.
-            let after_char_to_delete = self.value.chars().skip(self.char_idx);
-
-            // Put all characters together except the selected one.
-            // By leaving the selected one out, it is forgotten and therefore deleted.
-            self.value = before_char_to_delete.chain(after_char_to_delete).collect();
-            self.move_left();
-        }
-    }
-
-    /// Returns the byte index based on the character position.
-    ///
-    /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
-    /// the byte index based on the index of the character.
-    fn byte_idx(&self) -> usize {
-        self.value
-            .char_indices()
-            .map(|(i, _)| i)
-            .nth(self.char_idx)
-            .unwrap_or(self.value.len())
-    }
-
-    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
-        new_cursor_pos.clamp(0, self.value.chars().count())
-    }
-
-    pub fn delete(&mut self) {
-        // Method "remove" is not used on the saved text for deleting the selected char.
-        // Reason: Using remove on String works on bytes instead of the chars.
-        // Using remove would require special care because of char boundaries.
-        if self.char_idx != self.value.len() {
-            // Getting all characters before the selected character.
-            let before_char_to_delete = self.value.chars().take(self.char_idx);
-            // Getting all characters after selected character.
-            let after_char_to_delete = self.value.chars().skip(self.char_idx + 1);
-
-            // Put all characters together except the selected one.
-            // By leaving the selected one out, it is forgotten and therefore deleted.
-            self.value = before_char_to_delete.chain(after_char_to_delete).collect();
-        }
-    }
-
-    pub fn input(&mut self, new_char: char) {
-        let idx = self.byte_idx();
-        self.value.insert(idx, new_char);
-        self.move_right();
-    }
-
-    pub fn jump_to_first(&mut self) {
-        self.char_idx = 0;
-    }
-
-    pub fn jump_to_last(&mut self) {
-        self.char_idx = self.value.len();
-    }
-
-    pub fn move_left(&mut self) {
-        let cursor_moved_left = self.char_idx.saturating_sub(1);
-        self.char_idx = self.clamp_cursor(cursor_moved_left);
-    }
-
-    pub fn move_right(&mut self) {
-        let cursor_moved_right = self.char_idx.saturating_add(1);
-        self.char_idx = self.clamp_cursor(cursor_moved_right);
-    }
-
-    pub fn new() -> Self {
-        Self {
-            char_idx: 0,
-            value: String::new(),
-        }
-    }
-
-    pub fn submit(&mut self) -> String {
-        let input = self.value.clone();
-        self.char_idx = 0;
-        self.value.clear();
-        input
     }
 }
 
@@ -549,7 +405,7 @@ impl App {
             commands,
             help_menu_text,
             show_help_menu: false,
-            log_handle: ScrollableList::new(),
+            log_handle: ScrollableList::new(MAX_LOG_RECORDS),
             user_input: UserInput::new(),
         }
     }
---
+++ pp_client/src/app/widgets.rs
@@ -0,0 +1,171 @@
+use ratatui::{
+    self,
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
+    layout::{Alignment, Constraint, Flex, Layout, Margin, Position},
+    style::{Style, Stylize},
+    symbols::scrollbar,
+    text::{Line, Span, Text},
+    widgets::{
+        block, Block, Cell, Clear, List, ListDirection, ListItem, ListState, Padding, Paragraph,
+        Row, ScrollDirection, Scrollbar, ScrollbarOrientation, ScrollbarState, Table,
+    },
+    DefaultTerminal, Frame,
+};
+
+use std::{
+    collections::{HashSet, VecDeque},
+    io,
+    net::TcpStream,
+    sync::mpsc::{channel, Receiver, Sender},
+    thread,
+    time::{Duration, Instant},
+};
+
+/// Manages terminal messages and the terminal view position.
+pub struct ScrollableList {
+    max_items: usize,
+    pub list_items: VecDeque<ListItem<'static>>,
+    pub list_state: ListState,
+    pub scroll_state: ScrollbarState,
+}
+
+impl ScrollableList {
+    pub fn jump_to_first(&mut self) {
+        self.list_state.scroll_down_by(self.max_items as u16);
+        self.scroll_state.first();
+    }
+
+    pub fn jump_to_last(&mut self) {
+        self.list_state.scroll_up_by(self.max_items as u16);
+        self.scroll_state.last();
+    }
+
+    pub fn move_down(&mut self) {
+        self.list_state.scroll_up_by(1);
+        if self.list_state.selected().is_some() {
+            self.scroll_state.scroll(ScrollDirection::Forward);
+        }
+    }
+
+    pub fn move_up(&mut self) {
+        self.list_state.scroll_down_by(1);
+        if self.list_state.selected().is_some() {
+            self.scroll_state.scroll(ScrollDirection::Backward);
+        }
+    }
+
+    pub fn new(max_items: usize) -> Self {
+        Self {
+            max_items,
+            list_items: VecDeque::with_capacity(max_items),
+            list_state: ListState::default(),
+            scroll_state: ScrollbarState::new(0),
+        }
+    }
+
+    pub fn push(&mut self, item: ListItem<'static>) {
+        if self.list_items.len() == self.max_items {
+            self.list_items.pop_back();
+        }
+        self.list_items.push_front(item);
+        self.scroll_state = self.scroll_state.content_length(self.list_items.len());
+        self.move_down();
+    }
+}
+
+/// Manages user inputs at the terminal.
+pub struct UserInput {
+    /// Position of cursor in the input box.
+    pub char_idx: usize,
+    /// Current value of the input box.
+    pub value: String,
+}
+
+impl UserInput {
+    pub fn backspace(&mut self) {
+        // Method "remove" is not used on the saved text for deleting the selected char.
+        // Reason: Using remove on String works on bytes instead of the chars.
+        // Using remove would require special care because of char boundaries.
+        if self.char_idx != 0 {
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.value.chars().take(self.char_idx - 1);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.value.chars().skip(self.char_idx);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.value = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_left();
+        }
+    }
+
+    /// Returns the byte index based on the character position.
+    ///
+    /// Since each character in a string can be contain multiple bytes, it's necessary to calculate
+    /// the byte index based on the index of the character.
+    fn byte_idx(&self) -> usize {
+        self.value
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.char_idx)
+            .unwrap_or(self.value.len())
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.value.chars().count())
+    }
+
+    pub fn delete(&mut self) {
+        // Method "remove" is not used on the saved text for deleting the selected char.
+        // Reason: Using remove on String works on bytes instead of the chars.
+        // Using remove would require special care because of char boundaries.
+        if self.char_idx != self.value.len() {
+            // Getting all characters before the selected character.
+            let before_char_to_delete = self.value.chars().take(self.char_idx);
+            // Getting all characters after selected character.
+            let after_char_to_delete = self.value.chars().skip(self.char_idx + 1);
+
+            // Put all characters together except the selected one.
+            // By leaving the selected one out, it is forgotten and therefore deleted.
+            self.value = before_char_to_delete.chain(after_char_to_delete).collect();
+        }
+    }
+
+    pub fn input(&mut self, new_char: char) {
+        let idx = self.byte_idx();
+        self.value.insert(idx, new_char);
+        self.move_right();
+    }
+
+    pub fn jump_to_first(&mut self) {
+        self.char_idx = 0;
+    }
+
+    pub fn jump_to_last(&mut self) {
+        self.char_idx = self.value.len();
+    }
+
+    pub fn move_left(&mut self) {
+        let cursor_moved_left = self.char_idx.saturating_sub(1);
+        self.char_idx = self.clamp_cursor(cursor_moved_left);
+    }
+
+    pub fn move_right(&mut self) {
+        let cursor_moved_right = self.char_idx.saturating_add(1);
+        self.char_idx = self.clamp_cursor(cursor_moved_right);
+    }
+
+    pub fn new() -> Self {
+        Self {
+            char_idx: 0,
+            value: String::new(),
+        }
+    }
+
+    pub fn submit(&mut self) -> String {
+        let input = self.value.clone();
+        self.char_idx = 0;
+        self.value.clear();
+        input
+    }
+}
