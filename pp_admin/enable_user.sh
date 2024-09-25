39a5d8cb0fc6698f1c11c6e209a3657e647ef4e8
andrew <akberger20@gmail.com>
2024-09-25 12:56:47 -0500

    wrap key in quotes

--- pp_admin/enable_user.sh
+++ pp_admin/enable_user.sh
@@ -7,5 +7,5 @@ if [ -z "$PUBLIC_KEY" ]; then
     echo "Error: PUBLIC_KEY not set"
     exit 1
 fi
-echo $PUBLIC_KEY > $HOME/.ssh/authorized_keys \
+echo "$PUBLIC_KEY" > $HOME/.ssh/authorized_keys \
 && echo "Success"
